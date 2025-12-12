037ccba8319074be8eb2f9d1ae884b419e05c806
LilleAila <olai.solsvik@gmail.com>
2025-12-12 21:13:27 +0100

    snacks/image: init

---
+++ config/snacks.nix
@@ -0,0 +1,29 @@
+{ pkgs, ... }:
+{
+  vim = {
+    extraPackages = with pkgs; [
+      imagemagick
+      ghostscript
+      texliveFull
+      mermaid-cli
+    ];
+
+    utility.snacks-nvim = {
+      enable = true;
+      setupOpts = {
+        image = {
+          enabled = true;
+          force = true;
+          doc = {
+            conceal = false;
+            inline = false;
+            float = true;
+          };
+          math = {
+            enabled = true;
+          };
+        };
+      };
+    };
+  };
+}
